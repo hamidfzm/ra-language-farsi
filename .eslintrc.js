ace721474f70cd14fac486f5725fbc0697c693f9
Hamid FzM <hamidfzm@gmail.com>
2022-04-23 18:57:14 +0430

    feat: upgrade to react-admin 4
    BREAKING CHANGES
    - New translations
    - New development tools
    - Upgrade to ts 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  root: true,
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-  ],
-  parserOptions: {
-    sourceType: 'module',
-  },
-  env: {
-    es6: true,
-    node: true,
-    browser: true,
-    jest: true,
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: '@hamidfzm',
 };
